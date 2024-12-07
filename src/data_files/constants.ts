64956aad4223351f752af8cfce3260933e58c62f
devSaifur <saifur2akash@gmail.com>
2024-12-08 01:16:26 +0600

    setting up nav

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,12 +1,12 @@
 import ogImageSrc from "@images/social.png";
 
 export const SITE = {
-  title: "ScrewFast",
+  title: "BSS Contracting",
   tagline: "Top-quality Hardware Tools",
   description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
   description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  url: "https://bsscontracting.us",
+  author: "Saifur Rahman",
 };
 
 export const SEO = {
@@ -34,6 +34,6 @@ export const OG = {
   type: "website",
   url: SITE.url,
   title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  description: "Maintain your property with BSS Contracting top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
   image: ogImageSrc,
 };
