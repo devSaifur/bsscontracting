7eea38f1aa484a6817b06cccf1f73d3b7a55812d
devSaifur <saifur2akash@gmail.com>
2025-01-31 21:31:04 +0600

    more cleanup

--- src/data_files/services.ts
+++ src/data_files/services.ts
@@ -1,12 +1,13 @@
 // Import necessary images
-import blueprints from '@images/blueprints-image.avif'
-import personWorking from '@images/person-working.avif'
+
 import beforeAfter from '@images/before-after.avif'
-import constructionWorkers from '@images/construction-workers.avif'
-import aerialView from '@images/aerial-view.avif'
-import usingTools from '@images/using-tools.avif'
-import progressBuilding from '@images/progress-building.avif'
-import underConstruction from '@images/under-construction.avif'
+import treeService from '@images/tree-services-1.webp'
+import treeService2 from '@images/tree-services-2.webp'
+import lawnCare from '@images/lawn-care-1.webp'
+import lawnCare2 from '@images/lawn-care-2.webp'
+import interiorDesign from '@images/interior-design.webp'
+import fixSupport from '@images/fix-support-1.webp'
+import fixSupport2 from '@images/fix-support-2.webp'
 
 interface Article {
   isRightSection: boolean
@@ -27,14 +28,14 @@ interface Article {
 export const articles: Article[] = [
   {
     isRightSection: true,
-    title: 'Delivering Expert Guidance',
+    title: 'Tree Services by Trusted Arborist',
     subTitle:
-      'Embarking on a construction project can be overwhelming. With our professional consultation services, we guide you through every stage, ensuring you make informed decisions. Whether you are a DIY enthusiast or a skilled contractor, our experts are on hand to offer tailored advice on product selection, project scope, and compliance with local regulations.',
+      'BSS Contracting offers a comprehensive range of tree services to keep your property safe and beautiful. Our certified arborists provide expert tree care, including pruning, removal, and emergency services. We use the latest equipment and techniques to ensure the health and longevity of your trees.',
     single: false,
-    imgOne: blueprints,
-    imgOneAlt: 'Blueprints and digital tablet with construction plans.',
-    imgTwo: personWorking,
-    imgTwoAlt: 'Person working in the office'
+    imgOne: treeService,
+    imgOneAlt: 'Tree services by trusted arborist',
+    imgTwo: treeService2,
+    imgTwoAlt: 'Person working in the tree'
   },
   {
     isRightSection: false,
@@ -45,40 +46,39 @@ export const articles: Article[] = [
     imgAlt: 'Construction site before and after',
     btnExists: true,
     btnTitle: 'Learn More',
-    btnURL: '#'
+    btnURL: '/contact'
   },
   {
     isRightSection: true,
-    title: 'Navigating Projects with Professional Oversight',
+    title: 'Lawn Care and Maintenance Services',
     subTitle:
-      'Effective project management is at the heart of any successful build. ScrewFast provides thorough planning and robust management services that keep your project on time and within budget. Let us handle the complexities of workflow coordination, resource allocation, and stakeholder communication while you focus on your vision.',
+      'BSS Contracting provides professional lawn care and maintenance services to keep your property looking its best. Our team of experts offers a range of services, including mowing, edging, fertilization, and weed control. We use environmentally friendly products and techniques to ensure a healthy and vibrant lawn.',
     single: false,
-    imgOne: constructionWorkers,
-    imgOneAlt: 'Construction workers orchestrating a project',
-    imgTwo: aerialView,
-    imgTwoAlt: 'Aerial view of managed construction'
+    imgOne: lawnCare,
+    imgOneAlt: 'Lawn care and maintenance services',
+    imgTwo: lawnCare2,
+    imgTwoAlt: 'Person mowing the lawn'
   },
   {
     isRightSection: false,
-    title: 'Ensuring Long-lasting Performance',
+    title: 'Interior Design and Renovation Services',
     subTitle:
-      "Our commitment to your project doesn't end at completion. ScrewFast offers ongoing maintenance and support services to ensure your construction's longevity and performance. From regular check-ups to emergency assistance, our responsive team is there to provide seamless support.",
-    img: usingTools,
-    imgAlt:
-      'Man in orange and black vest wearing white helmet holding yellow and black power tool'
+      "BSS Contracting's interior design and renovation services are tailored to meet your unique needs and style preferences. Our team of experts works closely with you to create a space that reflects your personality and lifestyle. From concept to completion, we handle every aspect of the project with precision and care, ensuring a beautiful and functional result.",
+    img: interiorDesign,
+    imgAlt: 'Interior design and renovation services by BSS Contracting'
   },
   {
     isRightSection: true,
-    title: 'Crafting Bespoke Strategies for Unique Challenges',
+    title: 'Fixing & Support Services',
     subTitle:
-      'For our larger enterprise clients, ScrewFast offers custom solutions designed to meet specific industry challenges. By understanding your unique needs, we engineer tailored strategies aimed at optimizing your operations, enhancing efficiency, and driving your business forward.',
+      'Our team of skilled professionals provides reliable fixing and support services to address a wide range of issues in your home or business. From minor repairs to major renovations, we have the expertise and tools to get the job done right. We offer prompt and efficient service to ensure your property is safe and functional.',
     single: false,
-    imgOne: progressBuilding,
-    imgOneAlt: 'In progress building structure',
-    imgTwo: underConstruction,
-    imgTwoAlt: 'Brown and gray building under construction',
+    imgOne: fixSupport,
+    imgOneAlt: 'A man fixing electrical wires',
+    imgTwo: fixSupport2,
+    imgTwoAlt: 'Person fixing electrical wires',
     btnExists: true,
-    btnTitle: 'Read more',
-    btnURL: '#'
+    btnTitle: 'Learn More',
+    btnURL: '/contact'
   }
 ]
--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,40 +1,39 @@
 // An array of links for navigation bar
 const navBarLinks = [
-  { name: "Home", url: "/" },
-  { name: "Services", url: "/services" },
-  { name: "Contact", url: "/contact" },
-];
+  { name: 'Home', url: '/' },
+  { name: 'Services', url: '/services' },
+  { name: 'Contact', url: '/contact' }
+]
 // An array of links for footer
 const footerLinks = [
   {
-    section: "Ecosystem",
+    section: 'Ecosystem',
     links: [
-      { name: "Documentation", url: "/welcome-to-docs/" },
-      { name: "Tools & Equipment", url: "/products" },
-      { name: "Construction Services", url: "/services" },
-    ],
+      { name: 'Tools & Equipment', url: '/' },
+      { name: 'Construction Services', url: '/services' }
+    ]
   },
   {
-    section: "Company",
+    section: 'Company',
     links: [
-      { name: "About us", url: "#" },
-      { name: "Blog", url: "/blog" },
-      { name: "Careers", url: "#" },
-      { name: "Customers", url: "#" },
-    ],
-  },
-];
-// An object of links for social icons
+      { name: 'About us', url: '/' },
+      { name: 'Blog', url: '/' },
+      { name: 'Careers', url: '/' },
+      { name: 'Customers', url: '/' }
+    ]
+  }
+]
+
 const socialLinks = {
-  facebook: "https://www.facebook.com/",
-  x: "https://twitter.com/",
-  github: "https://github.com/mearashadowfax/ScrewFast",
-  google: "https://www.google.com/",
-  slack: "https://slack.com/",
-};
+  facebook: 'https://www.facebook.com/',
+  x: 'https://twitter.com/',
+  github: 'https://github.com/mearashadowfax/ScrewFast',
+  google: 'https://www.google.com/',
+  slack: 'https://slack.com/'
+}
 
 export default {
   navBarLinks,
   footerLinks,
-  socialLinks,
-};
+  socialLinks
+}
